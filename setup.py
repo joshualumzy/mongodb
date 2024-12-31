5372419aff3c38a6d6dde4b2c5ee896752e5ce82
Joshua Lum <joshualum@mac.lan>
2025-01-01 00:45:51 +0800

    everything updated

--- setup.py
+++ setup.py
@@ -1,15 +1,31 @@
+# Setup to create a distributable Python package
+
 from setuptools import setup, find_packages
 from typing import List
+
+HYPEN_E_DOT = "-e ."
+
+
+def get_requiremet(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
 __version__ = "0.0.4"
-REPO_NAME = "mongodbconnectorpkg"
-PKG_NAME = "databaseautomation"
-AUTHOR_USER_NAME = "sunnysavita10"
-AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+REPO_NAME = "mongodb"
+PKG_NAME = "MongoDB-Connect"
+AUTHOR_USER_NAME = "joshualumzy"
+AUTHOR_EMAIL = "166535332+joshualumzy@users.noreply.github.com"
 
 setup(
     name=PKG_NAME,
@@ -25,4 +41,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    install_requires=get_requiremet("./requirements_dev.txt"),
 )
