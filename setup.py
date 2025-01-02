fdb2d144550a724a1a6f913cd712b94dcd2fa63c
Joshua Lum <joshualum@mac.lan>
2025-01-02 17:08:26 +0800

    revert to last working setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 REPO_NAME = "mongodb"
 PKG_NAME = "databaseautomation"
 AUTHOR_USER_NAME = "joshualumzy"
@@ -34,12 +34,11 @@ setup(
     author_email=AUTHOR_EMAIL,
     description="A python package for connecting with database.",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=get_requiremet("./requirements_dev.txt"),
 )
