78d0f9dba1ba2d960d7dee8c30fe83d5d51f9c8a
Joshua Lum <joshualum@mac.lan>
2025-01-03 19:25:26 +0800

    update package name to dbautomator

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.2"
 REPO_NAME = "mongodb"
-PKG_NAME = "database_automation"
+PKG_NAME = "dbautomator"
 AUTHOR_USER_NAME = "joshualumzy"
 AUTHOR_EMAIL = "166535332+joshualumzy@users.noreply.github.com"
 
